85277eb9c50993b2e32c2b4f3a101d702e4ff648
Isaac Vidal Daza <isvida@gmail.com>
2024-05-25 21:33:04 +0200

    Comienzo presentación

--- langchain-ollama-chainlit/simple_chaiui.py
+++ langchain-ollama-chainlit/simple_chaiui.py
@@ -14,7 +14,7 @@ async def on_chat_start():
         [
             (
                 "system",
-                "You're a very knowledgeable historian who provides accurate and eloquent answers to historical questions.",
+                "You are a helpful assistant. You must always make responses to spanish question, if you recieve a question in any other language. Moreover, you have to answer in spanish."
             ),
             ("human", "{question}"),
         ]
